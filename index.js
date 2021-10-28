8ce0b63ab157bae32ef32ecd836acc4909871119
Priscila Ribeiro Monteiro <pririmonteiro@Priscilas-MBP.attlocal.net>
2021-10-28 10:02:44 -0700

    starting

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+
+const PORT = process.env.PORT || 5000
+
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>Web 47 rocks!</h1>
+  `)
+})
+
+app.listen(PORT, () => {
+  console.log(`listening on ${PORT}`)
+})
